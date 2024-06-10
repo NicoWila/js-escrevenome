08c2161cfc2dc020aa481d24ad8b47050e82d140
NicoWila <00001089022839sp@al.educacao.sp.gov.br>
2024-06-10 09:45:02 -0300

    readicionando arquivos js e css

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+let palavra;
+
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(400, 400);
+  let palavras = ['CaJú?', 'campo', 'GALINHA!'];
+  palavra = random(palavras);
+}
   
-  function draw() {
-    background(220);
+function draw() {
+  background("black");
+  textFont("courier new", 50);
+  fill("palegreen");
+  textAlign(CENTER, CENTER);
+  
+  let quantidade = map(mouseX, 0, width, 1, palavra.length);
+let parcial = palavra.substring(0,quantidade);
+text(parcial,200,200);
+  if (mouseX > 395){
+    text('>', 40, 250);
   }
+   
+  
+ describe('A text prompt from a game is written in a green, monospace font on a black background.');
+}
